72be8b348b8e892e410ea7df19480e9a5193b28b
Marcin Mazurek <marcin@mazurek.pro>
2017-03-31 18:25:33 +0200

    Add more params to getStockData

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const get = require('axios').get;
 
-function getStockData(symbols) {
-  const joinedSymbols = symbols.map(symbol => '"' + symbol + '"').join(',');
+function getStockData(symbols, startDate, endDate) {
+  const joinedSymbols = symbols.map(symbol => `"${symbol}"`).join(',');
 
   return new Promise((resolve, reject) => {
     return get('http://query.yahooapis.com/v1/public/yql', {
       params: {
-        q: 'select Symbol, Date, Close from yahoo.finance.historicaldata where symbol in (' + joinedSymbols + ') and startDate = "2017-01-01" and endDate = "2017-03-31"',
+        q: `select Symbol, Date, Close from yahoo.finance.historicaldata where symbol in (${joinedSymbols}) and startDate = "${startDate}" and endDate = "${endDate}"`,
         format: 'json',
         env: 'store://datatables.org/alltableswithkeys'
     }})
@@ -15,5 +15,5 @@ function getStockData(symbols) {
   });
 }
 
-getStockData(['AAPL', 'GOOG', 'AMZN'])
+getStockData(['AAPL', 'GOOG', 'AMZN'], '2017-01-01', '2017-03-31')
   .then(response => console.log(response));
